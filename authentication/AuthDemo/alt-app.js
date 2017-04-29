219766b494ba8f1dea0fc817727e678b887d3011
Alan Burkes <alan@Mac-mini.local>
2017-04-29 12:32:11 -0700

    Middleware failure.

--- authentication/AuthDemo/alt-app.js
+++ authentication/AuthDemo/alt-app.js
@@ -1,23 +1,21 @@
-var express                 = require("express"),
-    mongoose                = require("mongoose"),
-    passport                = require("passport"),
-    bodyParser              = require("body-parser"),
-    LocalStrategy           = require("passport-local"),
-    passportLocalMongoose   = require("passport-local-mongoose"),
-    User                    = require("./models/user");
-
-mongoose.connect("mongodb://localhost/authdemo");
-
-
-
+var express               = require("express"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"),
+    bodyParser            = require("body-parser"),
+    User                  = require("./models/user"),
+    LocalStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose")
+    
+mongoose.connect("mongodb://localhost/auth_demo_app");
 var app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(require("express-session")({
-    secret: "I like big butts and I cannot lie",
+    secret: "Rusty is the best and cutest dog in the world",
     resave: false,
     saveUninitialized: false
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -25,51 +23,55 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//============
+// ROUTES
+//============
 
 app.get("/", function(req, res){
     res.render("home");
 });
 
-
-app.get("/secret", isLoggedIn, function(req, res){
-    res.render("secret")
+app.get("/secret",isLoggedIn, function(req, res){
+   res.render("secret"); 
 });
 
-//AUTH ROUTES
+// Auth Routes
 
-// signup form
+//show sign up form
 app.get("/register", function(req, res){
-    res.render("register");
-})
-
+   res.render("register"); 
+});
+//handling user sign up
 app.post("/register", function(req, res){
     User.register(new User({username: req.body.username}), req.body.password, function(err, user){
         if(err){
             console.log(err);
-            res.render('register');
-        } else {
-            passport.authenticate("local")(req, res, function(){
-                res.redirect("/secret");
-            })
+            return res.render('register');
         }
-    })
-})
-
-//LOGIN ROUTES
-app.get("/login", function(req, res){
-    res.render("login");
+        passport.authenticate("local")(req, res, function(){
+           res.redirect("/secret");
+        });
+    });
 });
 
+// LOGIN ROUTES
+//render login form
+app.get("/login", function(req, res){
+   res.render("login"); 
+});
+//login logic
+//middleware
 app.post("/login", passport.authenticate("local", {
     successRedirect: "/secret",
     failureRedirect: "/login"
-}), function(req, res){
-})
-
-app.get("/logoff", function(req, res){
-    req.logout();
-    res.redirect("/");  
+}) ,function(req, res){
 });
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
 
 function isLoggedIn(req, res, next){
     console.log(req.isAuthenticated());
@@ -79,6 +81,7 @@ function isLoggedIn(req, res, next){
     res.redirect("/login");
 }
 
+
 app.listen(8000, "localhost", function(){
-    console.log("server started...");
-});
+    console.log("server started.......");
+})
